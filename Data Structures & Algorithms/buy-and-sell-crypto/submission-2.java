6f92fc3981ac58312fcbf46b267f3eed3d351826
Jerry Morales Lara <97256472+Gera39@users.noreply.github.com>
2026-09-20 16:02:51 -0600

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int result = 0;
+        int j = 0;
+        for(int i = 1; i < prices.length; i++){
+            if(prices[j]  < prices[i] ){
+                result = Math.max(result, (prices[i] - prices[j]));
+                continue;
+            }
+            else{
+            j = i; 
+            } 
+        }
+        return result;
+    }
+}
